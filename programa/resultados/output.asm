71e97fb962639a674c77affde3f33190226fcde8
JPHuntV <jeanpaulhunt16@gmail.com>
2023-06-03 16:01:20 -0600

    generacion mips, operaciones , declaraciones

--- programa/resultados/output.asm
+++ programa/resultados/output.asm
@@ -1,235 +1,528 @@
 .data
-arrays: .space 20
-c: .space 8
+float0: .float 1.2
+float1: .float 1.2
+float2: .float 2.3
+ttt: .space 1
+var: .space 1
+i: .word 0
+otrasS: .word 0
+xxx: .word 0
+otrasSsd: .word 0
+prueba: .word 0
+otras: .word 0
+otrase: .word 0
+var: .word 0
+i: .word 0
 a: .space 28
 b: .space 16
+float3: .float 6.7
 .text
 main:
 test:
-#declaracion de array
-t0 = 'a'
-arrays[0] = t0
-#declaracion de array
-t1 = 1
-t2 = 2
-c[0] = t1
-c[1] = t2
-dataChar t
-t3 = 'a'
-t = t3
-t4 = 'b'
-arrays[2] = t4
+#declaracion de float
+#declaracion de temporal
+#largo = 3
+l.s $f0, float0
+#declaracion de temporal
+#largo = 3
+sb $t0, t0
+#declaracion de temporal
+#largo = 3
+l.s $f1, float1
+#declaracion de temporal
+#largo = 3
+l.s $f2, float2
+#declaracion de temporal
+#largo = 5
+#op = +
+add $t0, $t-2, $t-1
+move $t1, $t0
+#declaracion de temporal
+#largo = 3
+sb $t1, t3
+#declaracion de char
+#declaracion de temporal
+#largo = 3
+li $t1, 'a'
+#declaracion de temporal
+#largo = 3
+li $t2, 'b'
+#declaracion de temporal
+#largo = 5
+#op = +
+add $t3, $t1, $t2
+move $t4, $t3
+#declaracion de temporal
+#largo = 3
+sb $t4, t6
+#declaracion de char
+#declaracion de temporal
+#largo = 3
+li $t4, 'a'
+sb $t4, var
+#declaracion de temporal
+#largo = 3
+li $t5, var
+#declaracion de temporal
+#largo = 3
+sb $t6, ttt
+#declaracion de temporal
+#largo = 5
+#op = +
+add $t6, $t4, $t5
+move $t7, $t6
+sb $t6, var
 _test_while1_eval:
-t5 = a
-t6 = b
-t7 = t5 > t6
-if t7 goto _test_while1_body
-goto _test_while1_end
+#declaracion de temporal
+#largo = 3
+li $t7, a
+#declaracion de temporal
+#largo = 3
+li $t8, b
+#declaracion de temporal
+#largo = 5
+#op = >
+sgt $t9, $t7, $t8
+#if
+beqz $t9, _test_while1_body
+#goto
+j _test_while1_end
 _test_while1_body:
 _test_for1_init:
-dataInt i
-t8 = 0
-i = t8
+#declaracion de int
+#declaracion de temporal
+#largo = 3
+li $t10, 0
+sb $t10, i
 _test_for1_eval:
-t9 = i
-t10 = a
-t11 = t9 > t10
-if t11 goto _test_for1_body
-goto _test_for1_end
+#declaracion de temporal
+#largo = 3
+li $t11, i
+#declaracion de temporal
+#largo = 3
+li $t12, a
+#declaracion de temporal
+#largo = 5
+#op = >
+sgt $t13, $t11, $t12
+#if
+beqz $t13, _test_for1_body
+#goto
+j _test_for1_end
 _test_for1_update:
-t12 = ++i
-goto _test_for1_eval
+#declaracion de temporal
+#largo = 3
+lw $t14, i
+addi $t15, $t14, 1
+sw $t15, i
+#goto
+j _test_for1_eval
 _test_for1_body:
-t13 = a
-t14 = 1
-t15 = t13 - t14
-a = t15
+#declaracion de temporal
+#largo = 3
+li $t15, a
+#declaracion de temporal
+#largo = 3
+li $t16, 1
+#declaracion de temporal
+#largo = 5
+#op = -
+sub $t17, $t15, $t16
+move $t18, $t17
+sb $t17, a
 _test_if1_eval:
-t16 = a
-t17 = 0
-t18 = t16 == t17
-if t18 goto _test_if1_body
-goto _test_if1_else
+#declaracion de temporal
+#largo = 3
+li $t18, a
+#declaracion de temporal
+#largo = 3
+li $t19, 0
+#declaracion de temporal
+#largo = 5
+#op = ==
+seq $t20, $t18, $t19
+#if
+beqz $t20, _test_if1_body
+#goto
+j _test_if1_else
 _test_if1_body:
-t19 = "entra al if"
-param t19
+#declaracion de temporal
+#largo = 5
+#op = al
+param t25
 call print, 1
-goto _test_if1_end
+#goto
+j _test_if1_end
 _test_if1_elifStm:
 _test_elif1_eval:
-t21 = true
-if t21 goto _test_elif1_body
-goto _test_elif1_else
+#declaracion de temporal
+#largo = 3
+sb $t21, true
+#if
+beqz $t20, _test_elif1_body
+#goto
+j _test_elif1_else
 _test_elif1_body:
-t22 = a
-param t22
+#declaracion de temporal
+#largo = 3
+li $t21, a
+param t28
 call print, 1
-goto _test_for1_end
-goto _test_elif1_end
+#goto
+j _test_for1_end
+#goto
+j _test_elif1_end
 _test_elif1_else:
 _test_elif2_eval:
-t24 = false
-t25 = true
-t26 = t24 ^ t25
-if t26 goto _test_elif2_body
-goto _test_elif2_else
+#declaracion de temporal
+#largo = 3
+li $t22, false
+#declaracion de temporal
+#largo = 3
+sb $t23, true
+#declaracion de temporal
+#largo = 5
+#op = ^
+and $t23, $t21, $t22
+#if
+beqz $t23, _test_elif2_body
+#goto
+j _test_elif2_else
 _test_elif2_body:
-t27 = a
-param t27
+#declaracion de temporal
+#largo = 3
+li $t24, a
+param t33
 call read, 1
-goto _test_for1_end
-goto _test_elif2_end
+#goto
+j _test_for1_end
+#goto
+j _test_elif2_end
 _test_elif2_else:
-goto _test_elif2_end
+#goto
+j _test_elif2_end
 _test_elif2_end:
-goto _test_elif1_end
+#goto
+j _test_elif1_end
 _test_elif1_end:
 _test_if1_else:
 _test_else1_body:
-t29 = 0
-i = t29
-goto _test_else1_end
+#declaracion de temporal
+#largo = 3
+li $t25, 0
+sb $t25, i
+#goto
+j _test_else1_end
 _test_else1_end:
-goto _test_if1_end
+#goto
+j _test_if1_end
 _test_if1_end:
-goto _test_for1_update
+#goto
+j _test_for1_update
 _test_for1_end:
-goto _test_while1_eval
+#goto
+j _test_while1_eval
 _test_while1_end:
-t30 = a
-t31 = b
-t32 = t30 + t31
-return t32
+#declaracion de temporal
+#largo = 3
+li $t26, a
+#declaracion de temporal
+#largo = 3
+li $t27, b
+#declaracion de temporal
+#largo = 5
+#op = +
+add $t28, $t26, $t27
+move $t29, $t28
+return t38
 miFunc:
-dataInt otrasS
-t33 = 1
-otrasS = t33
-dataInt otrasSsd
-t34 = 2
-t35 = 3
-t36 = 4
-t37 = t35 + t36
-t38 = t34 + t37
-otrasSsd = t38
-dataInt prueba
-t39 = 1
-t40 = otrasS
-param t39
-param t40
-t41 = call test, 2
-prueba = t41
-dataInt otras
-t42 = -dif
-otras = t42
-dataInt otrase
-t43 = -2
-otrase = t43
+#declaracion de int
+#declaracion de temporal
+#largo = 3
+li $t29, 1
+sb $t29, otrasS
+#declaracion de temporal
+#largo = 3
+li $t30, 2
+#declaracion de temporal
+#largo = 3
+li $t31, otrasS
+#declaracion de temporal
+#largo = 5
+#op = +
+add $t32, $t30, $t31
+move $t33, $t32
+sb $t32, otrasS
+#declaracion de int
+#declaracion de temporal
+#largo = 3
+li $t33, otrasS
+#declaracion de temporal
+#largo = 3
+li $t34, 2
+#declaracion de temporal
+#largo = 5
+#op = +
+add $t35, $t33, $t34
+move $t36, $t35
+sb $t35, xxx
+#declaracion de int
+#declaracion de temporal
+#largo = 3
+li $t36, 2
+#declaracion de temporal
+#largo = 3
+li $t37, 3
+#declaracion de temporal
+#largo = 3
+li $t38, 4
+#declaracion de temporal
+#largo = 5
+#op = +
+add $t39, $t37, $t38
+move $t40, $t39
+#declaracion de temporal
+#largo = 5
+#op = +
+add $t40, $t38, $t39
+move $t41, $t40
+sb $t40, otrasSsd
+#declaracion de int
+#declaracion de temporal
+#largo = 3
+li $t41, 1
+#declaracion de temporal
+#largo = 3
+li $t42, otrasS
+param t51
+param t52
+#declaracion de temporal
+#largo = 5
+#op = test,
+sb $t42, prueba
+#declaracion de int
+#declaracion de temporal
+#largo = 3
+li $t43, -dif
+sb $t43, otras
+#declaracion de int
+#declaracion de temporal
+#largo = 3
+li $t44, -2
+sb $t44, otrase
 dataString str
-t44 = "Hola $%&/#$&) mundo"
-str = t44
-dataInt var
-t45 = 8
-var = t45
+#declaracion de temporal
+#largo = 5
+#op = $%&/#$&)
+sb $t44, str
+#declaracion de int
+#declaracion de temporal
+#largo = 3
+li $t45, 8
+sb $t45, var
 _miFunc_for1_init:
-dataInt i
-t46 = 0
-i = t46
+#declaracion de int
+#declaracion de temporal
+#largo = 3
+li $t46, 0
+sb $t46, i
 _miFunc_for1_eval:
-t47 = i
-t48 = var
-t49 = t47 > t48
-if t49 goto _miFunc_for1_body
-goto _miFunc_for1_end
+#declaracion de temporal
+#largo = 3
+li $t47, i
+#declaracion de temporal
+#largo = 3
+li $t48, var
+#declaracion de temporal
+#largo = 5
+#op = >
+sgt $t49, $t47, $t48
+#if
+beqz $t49, _miFunc_for1_body
+#goto
+j _miFunc_for1_end
 _miFunc_for1_update:
-t50 = ++i
-goto _miFunc_for1_eval
+#declaracion de temporal
+#largo = 3
+lw $t50, i
+addi $t51, $t50, 1
+sw $t51, i
+#goto
+j _miFunc_for1_eval
 _miFunc_for1_body:
 _miFunc_doWhile1_body:
-t51 = var
-t52 = 1
-t53 = t51 - t52
-var = t53
+#declaracion de temporal
+#largo = 3
+li $t51, var
+#declaracion de temporal
+#largo = 3
+li $t52, 1
+#declaracion de temporal
+#largo = 5
+#op = -
+sub $t53, $t51, $t52
+move $t54, $t53
+sb $t53, var
 _miFunc_doWhile1_eval:
-t54 = var
-t55 = 12
-t56 = t54 > t55
-t57 = 34
-t58 = 34
-t59 = t57 + t58
-t60 = 12
-t61 = t59 > t60
-t62 = t56 # t61
-if t62 goto _miFunc_doWhile1_body
-goto _miFunc_doWhile1_end
+#declaracion de temporal
+#largo = 3
+li $t54, var
+#declaracion de temporal
+#largo = 3
+li $t55, 12
+#declaracion de temporal
+#largo = 5
+#op = >
+sgt $t56, $t54, $t55
+#declaracion de temporal
+#largo = 3
+li $t57, 34
+#declaracion de temporal
+#largo = 3
+li $t58, 34
+#declaracion de temporal
+#largo = 5
+#op = +
+add $t59, $t57, $t58
+move $t60, $t59
+#declaracion de temporal
+#largo = 3
+li $t60, 12
+#declaracion de temporal
+#largo = 5
+#op = >
+sgt $t61, $t59, $t60
+#declaracion de temporal
+#largo = 5
+#op = #
+or $t62, $t60, $t61
+#if
+beqz $t62, _miFunc_doWhile1_body
+#goto
+j _miFunc_doWhile1_end
 _miFunc_doWhile1_end:
 _miFunc_if2_eval:
-t63 = var
-t64 = 0
-t65 = t63 == t64
-if t65 goto _miFunc_if2_body
-goto _miFunc_if2_else
+#declaracion de temporal
+#largo = 3
+li $t63, var
+#declaracion de temporal
+#largo = 3
+li $t64, 0
+#declaracion de temporal
+#largo = 5
+#op = ==
+seq $t65, $t63, $t64
+#if
+beqz $t65, _miFunc_if2_body
+#goto
+j _miFunc_if2_else
 _miFunc_if2_body:
-t66 = "entra al if"
-param t66
+#declaracion de temporal
+#largo = 5
+#op = al
+param t78
 call print, 1
-goto _miFunc_if2_end
+#goto
+j _miFunc_if2_end
 _miFunc_if2_elifStm:
 _miFunc_elif3_eval:
-t68 = true
-if t68 goto _miFunc_elif3_body
-goto _miFunc_elif3_else
+#declaracion de temporal
+#largo = 3
+sb $t66, true
+#if
+beqz $t65, _miFunc_elif3_body
+#goto
+j _miFunc_elif3_else
 _miFunc_elif3_body:
-t69 = var
-param t69
+#declaracion de temporal
+#largo = 3
+li $t66, var
+param t81
 call print, 1
-goto _miFunc_elif3_end
+#goto
+j _miFunc_elif3_end
 _miFunc_elif3_else:
 _miFunc_elif4_eval:
-t71 = false
-t72 = true
-t73 = t71 ^ t72
-if t73 goto _miFunc_elif4_body
-goto _miFunc_elif4_else
+#declaracion de temporal
+#largo = 3
+li $t67, false
+#declaracion de temporal
+#largo = 3
+sb $t68, true
+#declaracion de temporal
+#largo = 5
+#op = ^
+and $t68, $t66, $t67
+#if
+beqz $t68, _miFunc_elif4_body
+#goto
+j _miFunc_elif4_else
 _miFunc_elif4_body:
-t74 = var
-param t74
+#declaracion de temporal
+#largo = 3
+li $t69, var
+param t86
 call read, 1
-goto _miFunc_for1_end
-goto _miFunc_elif4_end
+#goto
+j _miFunc_for1_end
+#goto
+j _miFunc_elif4_end
 _miFunc_elif4_else:
-goto _miFunc_elif4_end
+#goto
+j _miFunc_elif4_end
 _miFunc_elif4_end:
-goto _miFunc_elif3_end
+#goto
+j _miFunc_elif3_end
 _miFunc_elif3_end:
-goto _miFunc_if2_end
+#goto
+j _miFunc_if2_end
 _miFunc_if2_end:
-goto _miFunc_for1_update
+#goto
+j _miFunc_for1_update
 _miFunc_for1_end:
-t76 = 1
-t77 = otrasS
-param t76
-param t77
+#declaracion de temporal
+#largo = 3
+li $t70, 1
+#declaracion de temporal
+#largo = 3
+li $t71, otrasS
+param t88
+param t89
 call test, 2
-return t78
+return t90
 miOtraFun:
-t79 = true
-return t79
+#declaracion de temporal
+#largo = 3
+sb $t72, true
+return t91
 main:
 #declaracion de array
-t80 = 1
-t81 = 2
-a[0] = t80
-a[1] = t81
+#declaracion de temporal
+#largo = 3
+li $t72, 1
+#declaracion de temporal
+#largo = 3
+li $t73, 2
+sb $t73, a[0]
+sb $t73, a[1]
 #declaracion de array
-b = t81
-t82 = "hola"
-param t82
+sb $t73, b
+#declaracion de temporal
+#largo = 3
+li $t74, "hola"
+param t94
 call print, 1
-t84 = 34
-param t84
+#declaracion de temporal
+#largo = 3
+li $t75, 34
+param t96
 call print, 1
-t86 = 6.7
-param t86
+#declaracion de temporal
+#largo = 3
+l.s $f3, float3
+param t98
 call print, 1
-t88 = 1
-return t88
+#declaracion de temporal
+#largo = 3
+li $t76, 1
+return t100
--- programa/src/MIPSCodeGenerator.java
+++ programa/src/MIPSCodeGenerator.java
@@ -5,12 +5,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import jflex.core.RegExp;
+
 public class MIPSCodeGenerator {
 
     private static FileWriter writer;
     private StringBuilder code;
     String dotData = ".data\n";
     int tempCount = 0;
+    int floatCount = 0;
+    int floatTempCount = 0;
+    boolean[] tRegisters = new boolean[10];
 
     public void generateMips() {
         code = new StringBuilder();
@@ -68,14 +73,125 @@ public class MIPSCodeGenerator {
     private void writeInstruction(String instruction) throws IOException {
         // Translate the instruction to MIPS and write it to the file
         // Handle each instruction accordingly
+        String[] data = instruction.split(" ");
+        int largo = data.length;
         if(instruction.startsWith("dataArray")){
-            code.append("#declaracion de array\n");
-            String[] data = instruction.split(" ");
+            code.append("#declaracion de array\n");     
             String nombreArray = data[1].substring(0, data[1].indexOf("["));
             String size = data[1].substring(data[1].indexOf("[")+1, data[1].indexOf("]"));
             dotData += nombreArray + ": .space " + Integer.toString(Integer.parseInt(size)*4) + "\n";
             return;
         }
+        else if(instruction.startsWith("dataChar")){
+            code.append("#declaracion de char\n");
+            String nombreChar = data[1];
+            dotData += nombreChar + ": .space 1\n";
+            return;
+        }
+        else if(instruction.startsWith("dataInt")){
+            code.append("#declaracion de int\n");
+            String nombreInt = data[1];
+            dotData += nombreInt + ": .word 0\n";
+            return;
+        }
+        else if(instruction.startsWith("dataFloat")){
+            code.append("#declaracion de float\n");
+            return;
+        }
+        else if(instruction.startsWith("if")){
+            code.append("#if\n");
+            code.append("beqz $t" + (tempCount-1) + ", " + data[3] + "\n");
+        }
+        else if(instruction.startsWith("goto")){
+            code.append("#goto\n");
+            code.append("j " + data[1] + "\n");
+        }
+        else if(instruction.startsWith("t")){
+            code.append("#declaracion de temporal\n");
+            code.append("#largo = " +data.length + "\n");
+            if(largo == 3){
+                if(data[2].startsWith("++")){
+                    code.append("lw $t" + tempCount + ", " + data[2].substring(2) + "\n");
+                    tempCount++;
+                    code.append("addi $t" + tempCount + ", $t" + (tempCount-1) + ", 1\n");
+                    code.append("sw $t" + tempCount + ", " + data[2].substring(2) + "\n");
+                    return;
+                }
+                else if(data[2].startsWith("'") || isStringInteger(data[2])){
+                    code.append("li $t" + tempCount + ", " + data[2] + "\n");
+                    //code.append("sb $t" + tempCount + ", " + data[0] + "\n");
+                    tempCount++;
+                }
+                else if(isStringFloat(data[2])){
+                    dotData += "float" + floatCount + ": .float " + data[2] + "\n";
+                    code.append("l.s $f" + floatTempCount + ", float" + floatCount + "\n");
+                    floatCount++;
+                    floatTempCount++;
+                }
+                else if(instruction.contains("= t") && largo == 3){
+                    code.append("sb $t" + (tempCount) + ", " + data[2] + "\n");
+                }else{
+                    code.append("li $t" + tempCount + ", " + data[2] + "\n");
+                    //code.append("sb $t" + tempCount + ", " + data[0] + "\n");
+                    tempCount++;
+                }
+            }
+            else if(largo == 5){
+                String op = data[3];
+                code.append("#op = " + op + "\n");
+                if(op.equals("+")){
+                    code.append("add $t" + tempCount + ", $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
+                    tempCount++;
+                    code.append("move $t" + tempCount + ", $t" + (tempCount-1) + "\n");
+                    //code.append("sb $t" + tempCount + ", " + data[0] + "\n");
+                }
+                else if(op.equals("-")){
+                    code.append("sub $t" + tempCount + ", $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
+                    tempCount++;
+                    code.append("move $t" + tempCount + ", $t" + (tempCount-1) + "\n");
+                    //code.append("sb $t" + tempCount + ", " + data[0] + "\n");                
+                }
+                else if(op.equals(">")){
+                    code.append("sgt $t" + tempCount + ", $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
+                    tempCount++;
+                }
+                else if(op.equals("<")){
+                    code.append("slt $t" + tempCount + ", $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
+                    tempCount++;
+                }
+                else if(op.equals("==")){
+                    code.append("seq $t" + tempCount + ", $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
+                    tempCount++;
+                }
+                else if(op.equals("!=")){
+                    code.append("sne $t" + tempCount + ", $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
+                    tempCount++;
+                }
+                else if(op.equals("^")){
+                    code.append("and $t" + tempCount + ", $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
+                    tempCount++;
+                }
+                else if(op.equals("#")){
+                    code.append("or $t" + tempCount + ", $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
+                    tempCount++;
+                }
+                else if(op.equals("*")){
+                    code.append("mult $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
+                    code.append("mflo $t" + tempCount + "\n");
+                    tempCount++;
+                }
+                else if(op.equals("/")){
+                    code.append("div $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
+                    code.append("mflo $t" + tempCount + "\n");
+                    tempCount++;
+                }
+            }
+            return;
+        }
+        else if(instruction.contains("= t") && largo == 3){
+            code.append("sb $t" + (tempCount-1) + ", " + data[0] + "\n");
+        }
+
         else{
 
             code.append(instruction + "\n");
@@ -91,5 +207,23 @@ public class MIPSCodeGenerator {
         }
     
         return text;
-      }
+    }
+
+    public static boolean isStringInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isStringFloat(String str) {
+        try {
+            Float.parseFloat(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
