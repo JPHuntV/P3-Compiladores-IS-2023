caf27aa2bb971b1b8409c609750b26941adb3933
JPHuntV <jeanpaulhunt16@gmail.com>
2023-06-05 13:14:16 -0600

    mips generation , capaz de traducir una suma e imprimirla correctamente

--- programa/src/MIPSCodeGenerator.java
+++ programa/src/MIPSCodeGenerator.java
@@ -4,6 +4,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.*;
+
 
 import jflex.core.RegExp;
 
@@ -16,9 +21,19 @@ public class MIPSCodeGenerator {
     int floatCount = 0;
     int floatTempCount = 0;
     boolean[] tRegisters = new boolean[10];
+    Map<String, String> dataTypesLoads = new HashMap<String, String>();
+    Map<String, String> dataTypesStores = new HashMap<String, String>();
+    Map<String, String> dotDatMap = new HashMap<String, String>();
+    Map<String, String> tempValues = new HashMap<String, String>();
+    //calling function stack list
+    String[] callingFunctionStack = new String[100];
+    ArrayList<String> paramStack = new ArrayList<String>();
+    String currentDataType = "";
 
     public void generateMips() {
         code = new StringBuilder();
+        InicializarDiccionarios();
+
         String codigoIntermedio = getCode();
         traducir(codigoIntermedio);
     }
@@ -46,7 +61,7 @@ public class MIPSCodeGenerator {
     private void escribirEncabezado() throws IOException {
         //writer.write(".data\n");
         code.append(".text\n");
-        code.append("main:\n");
+        code.append(".globl main\n");
     }
 
     private void traducirCodigo(String code) throws IOException {
@@ -92,6 +107,8 @@ public class MIPSCodeGenerator {
             code.append("#declaracion de int\n");
             String nombreInt = data[1];
             dotData += nombreInt + ": .word 0\n";
+            dotDatMap.put(nombreInt, "int");
+            currentDataType = "int";
             return;
         }
         else if(instruction.startsWith("dataFloat")){
@@ -105,6 +122,36 @@ public class MIPSCodeGenerator {
         else if(instruction.startsWith("goto")){
             code.append("#goto\n");
             code.append("j " + data[1] + "\n");
+        }
+        else if(instruction.startsWith("return")){
+            if(callingFunctionStack[callingFunctionStack.length-1] != null){
+                code.append("#return\n");
+                code.append("j " + callingFunctionStack[callingFunctionStack.length-1] + "\n");
+            }
+            else{
+                code.append("#return\n");
+                code.append("li $v0, 10\n");
+                code.append("syscall\n");
+            }
+        }
+        else if(instruction.startsWith("param")){
+            paramStack.add(data[1]);
+        }
+        else if(instruction.startsWith("call")){
+            if(data[1].equals("print,")){
+                code.append("#print\n");
+                String param = paramStack.remove(paramStack.size()-1);
+                System.out.println(param);
+                //code.append( dataTypesLoads.get(dotDatMap.get(tempValues.get(param)))+", "+tempValues.get(param)+ " ---\n");
+                code.append("move $a0, $" + param+ "\n");
+                //code.append("li $v0, 1\n");
+                code.append("li $v0, 1\n");
+                code.append("syscall\n");
+            }
+
+
+
+                
         }
         else if(instruction.startsWith("t")){
             code.append("#declaracion de temporal\n");
@@ -131,8 +178,9 @@ public class MIPSCodeGenerator {
                 else if(instruction.contains("= t") && largo == 3){
                     code.append("sb $t" + (tempCount) + ", " + data[2] + "\n");
                 }else{
-                    code.append("li $t" + tempCount + ", " + data[2] + "\n");
+                    code.append(dataTypesLoads.get(dotDatMap.get(data[2])) + " $t" + tempCount + ", " + data[2] + "\n");
                     //code.append("sb $t" + tempCount + ", " + data[0] + "\n");
+                    tempValues.put("t" + tempCount, data[2]);
                     tempCount++;
                 }
             }
@@ -142,7 +190,6 @@ public class MIPSCodeGenerator {
                 if(op.equals("+")){
                     code.append("add $t" + tempCount + ", $t" + (tempCount-2) + ", $t" + (tempCount-1) + "\n");
                     tempCount++;
-                    code.append("move $t" + tempCount + ", $t" + (tempCount-1) + "\n");
                     //code.append("sb $t" + tempCount + ", " + data[0] + "\n");
                 }
                 else if(op.equals("-")){
@@ -188,14 +235,20 @@ public class MIPSCodeGenerator {
             }
             return;
         }
-        else if(instruction.contains("= t") && largo == 3){
-            code.append("sb $t" + (tempCount-1) + ", " + data[0] + "\n");
+        else if(asignacionTemp(instruction) && largo == 3){
+            code.append(dataTypesStores.get(currentDataType)+" $t" + (tempCount-1) + ", " + data[0] + "\n");
         }
 
         else{
 
             code.append(instruction + "\n");
         }
+    }
+
+    public boolean asignacionTemp(String instruction){
+        Pattern pattern = Pattern.compile("= t\\d+");
+        Matcher matcher = pattern.matcher(instruction);
+        return matcher.find();
     }
 
     public static String readFileAsString(String fileName) {
@@ -224,6 +277,23 @@ public class MIPSCodeGenerator {
             return true;
         } catch (NumberFormatException e) {
             return false;
+        }
+    }
+
+    public void InicializarDiccionarios(){
+        dataTypesLoads.put("int", "lw");
+        dataTypesLoads.put("float", "l.s");
+        dataTypesLoads.put("char", "lb");
+        dataTypesLoads.put("String", "la");
+
+        dataTypesStores.put("int", "sw");
+        dataTypesStores.put("float", "s.s");
+        dataTypesStores.put("char", "sb");
+        dataTypesStores.put("String", "la");
+
+
+        for(int i = 0; i < 11; i++){
+            this.tempValues.put("t" + i, "");
         }
     }
 }
--- programa/src/main.java
+++ programa/src/main.java
@@ -2,7 +2,7 @@ package src;
 public class main {
     public static void main(String[] args) {
         App app = new App();
-        app.analizar("programa/src/test.txt");//lexer
-        app.parsear("programa/src/test.txt");//parser
+        app.analizar("programa/src/testCode.txt");//lexer
+        app.parsear("programa/src/testCode.txt");//parser
     }  
 }
